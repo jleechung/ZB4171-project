5a57984bd1f53b28029f8fb7980f4d7a1cdac20b
108-collab <65392342+108-collab@users.noreply.github.com>
2021-11-07 16:10:06 +0800

    Update data_distribution.R

--- code/analysis/data_distribution.R
+++ code/analysis/data_distribution.R
@@ -4,5 +4,5 @@ counts=read.csv("hypo_ani1_counts.csv")
 c<-data.matrix(counts)
 r<- round(c)  # round off the counts to nearest integer value 
 set.seed(4171)
-descdist(c[1,],boot=1000,discrete = TRUE)
-fidist(r[1,],"nbinom")
+descdist(c[5424,],boot=1000,discrete = TRUE)     #Ependymal
+fidist(r[5424,],"nbinom")

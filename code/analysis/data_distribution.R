b9939a8d3c671267536b4ffa165512c6943863b0
108-collab <65392342+108-collab@users.noreply.github.com>
2021-11-07 16:28:16 +0800

    Update data_distribution.R

--- code/analysis/data_distribution.R
+++ code/analysis/data_distribution.R
@@ -5,4 +5,12 @@ c<-data.matrix(counts)
 r<- round(c)  # round off the counts to nearest integer value 
 set.seed(4171)
 descdist(c[5424,],boot=1000,discrete = TRUE)     #Ependymal
-fidist(r[5424,],"nbinom")
+par(mfrow = c(2, 2))
+r5424<-fidist(r[5424,],"nbinom")
+plot.legend<-c("nbinom")
+denscomp(r5424, legendtext = plot.legend)
+qqcomp(r5424, legendtext = plot.legend)
+cdfcomp(r5424, legendtext = plot.legend)
+ppcomp(r5424, legendtext = plot.legend)
+
+

baf397941326075fa717c7390a1c2a60522b749a
108-collab <65392342+108-collab@users.noreply.github.com>
2021-11-12 01:21:08 +0800

    Update data_distribution.R

--- analysis/data_distribution.R
+++ analysis/data_distribution.R
@@ -4,7 +4,7 @@ counts=read.csv("hypo_ani1_counts.csv")
 c<-data.matrix(counts)
 r<- round(c)  # round off the counts to nearest integer value 
 set.seed(4171)
-descdist(c[5424,],boot=1000,discrete = TRUE)     #Ependymal
+descdist(r[5424,],boot=1000,discrete = TRUE)     #Ependymal, the value can be changed to plot cells from other celltypes
 par(mfrow = c(2, 2))
 r5424<-fidist(r[5424,],"nbinom")
 plot.legend<-c("nbinom")
@@ -13,8 +13,8 @@ qqcomp(r5424, legendtext = plot.legend)
 cdfcomp(r5424, legendtext = plot.legend)
 ppcomp(r5424, legendtext = plot.legend)
 
-c1<-c(203,433,439,472,505,516,526,555,610,696,699,711,717,759,762,778,1070,1263,1605,1610,1612,1618,1620,1622,1624,1639,1654,1661,1682,1819,2647,2708,2756,2757,3026,3352,3411,3844,3909,4144,4669,4692,5054,5091,5093,5095,5101,5125,5217,5218,5232,5508) # indexes of cell that belong to a particular cell type 
-OD_mature4<-r[c1,] ## to extract rows that belong to a particular cell subtype in this case ODmature4
+R=read.csv("hypo_ani1_counts.csv1")
+OD_mature4<-subset(R,Cell_class=="OD Mature 4")
 rowOD_mature4<-rowSums(OD_mature4)
 quantile(rowOD_mature4)
 index<-numeric(20)

4a3fc12e71e73bd6f02803b8c792ee6f1ce1abc4
108-collab <65392342+108-collab@users.noreply.github.com>
2021-11-15 19:31:46 +0800

    Update data_distribution.R

--- analysis/data_distribution.R
+++ analysis/data_distribution.R
@@ -13,38 +13,33 @@ qqcomp(r5424, legendtext = plot.legend)
 cdfcomp(r5424, legendtext = plot.legend)
 ppcomp(r5424, legendtext = plot.legend)
 
-R=read.csv("hypo_ani1_counts.csv1")
-OD_mature4<-subset(R,Cell_class=="OD Mature 4")
-rowOD_mature4<-rowSums(OD_mature4)
-quantile(rowOD_mature4)
-index<-numeric(20)
-q1=0
-q2=0
-q3=0
-q4=0
-c=0
-while(c<20){
-for (i in 1:length(rowOD_mature4)){
-  if(rowOD_mature4[i]<=292.5 & count1<5){
-    q1=q1+1
-    c=c+1
-    index[c]=i
-  }else if(rowOD_mature4[i]<=339.5 & rowOD_mature4[i]>292.5 & count2<5){
-    q2=q2+1
-    c=c+1
-    index[c]=i
-  }else if(rowOD_mature4[i]>339.5 & rowOD_mature4[i]<=380.0 & count3<5){
-    q3=q3+1
-    c=c+1
-    index[c]=i
-  }else if(count4<5 & rowOD_mature4[i]>380.0){
-    q4=q4+1
-    c=c+1
-    index[c]=i
-  }
-}
-}
-r[index,] #to get indexes of the 20 sampled cells from the original count matrix.
+R=read.csv("hypo_ani1_counts.csv1")            #csv with cell_class column from metadata added.
+ODMature4<-subset(R,Cell_class=="OD Mature 4") 
+ODMature4<-subset(ODMature4,select=-c(Cell_class))
+ODMature1<-subset(R,Cell_class=="OD Mature 1") 
+ODMature1<-subset(ODMature1,select=-c(Cell_class))
+ODMature2<-subset(R,Cell_class=="OD Mature 2") 
+ODMature2<-subset(ODMature2,select=-c(Cell_class))
+Endo3<-subset(R,Cell_class=="Endothelial 3")
+Endo3<-subset(Endo3,select=-c(Cell_class))
+Endo2<-subset(R,Cell_class=="Endothelial 2")
+Endo2<-subset(Endo2,select=-c(Cell_class))
+Endo1<-subset(R,Cell_class=="Endothelial 1")
+Endo1<-subset(Endo1,select=-c(Cell_class))
+Excitatory<-subset(R,Cell_class=="Excitatory")
+Excitatory<-subset(Excitatory,select=-c(Cell_class))
+Microglia<-subset(R,Cell_class=="Microglia")
+Microglia<-subset(Microglia,select=-c(Cell_class))
+Inhibitory<-subset(R,Cell_class=="Inhibitory")
+Inhibitory<-subset(Inhibitory,select=-c(Cell_class))
+Ambiguous<-subset(R,Cell_class=="Ambiguous")
+Ambiguous<-subset(Ambiguous,select=-c(Cell_class))
+Peri<-subset(R,Cell_class=="Pericytes")
+Peri<-subset(Peri,select=-c(Cell_class))
+Astrocyte<-subset(R,Cell_class=="Astrocyte")
+Astrocyte<-subset(Astrocyte,select=-c(Cell_class))
+
+
 
 install.packages(matrixStats)        
 library(matrixStats)
@@ -53,3 +48,39 @@ Variance<-(colSds(r))^2
 plot(log2(Mean+1), log2(Variance+1),pch=19, cex=0.5)
 abline(a=0,b=1,lty=3,col=2)
  
+
+ll_nbinom<-rep(NA, nrow(r))
+ll_pois<-rep(NA, nrow(r))
+for (i in 1:nrow(r)) {
+  if (i %% 100 == 0) message('Iteration ', i)
+  fit_nbinom <- fitdist(r[i,],'nbinom')
+  fit_pois <- fitdist(r[i,],'pois')
+  ll_nbinom[i] <- summary(fit_nbinom)[[6]]
+  ll_pois[i] <- summary(fit_pois)[[6]]
+}
+plot(y=ll_pois, x=ll_nbinom,pch=19,cex=0.5,col=rgb(0,0,0,0.5))
+abline(a=0,b=1,col=2)
+
+AIC_nbinom<-rep(NA, nrow(r))
+AIC_pois<-rep(NA, nrow(r))
+for (i in 1:nrow(r)) {
+  if (i %% 100 == 0) message('Iteration ', i)
+  fit_nbinom <- fitdist(r[i,],'nbinom')
+  fit_pois <- fitdist(r[i,],'pois')
+  AIC_nbinom[i] <- summary(fit_nbinom)[[7]]
+  AIC_pois[i] <- summary(fit_pois)[[7]]
+}
+plot(y=AIC_pois, x=AIC_nbinom,pch=19,cex=0.5,col=rgb(0,0,0,0.5))
+abline(a=0,b=1,col=2)
+
+BIC_nbinom<-rep(NA, nrow(r))                                                  
+BIC_pois<-rep(NA, nrow(r))
+for (i in 1:nrow(r)) {
+  if (i %% 100 == 0) message('Iteration ', i)
+  fit_nbinom <- fitdist(r[i,],'nbinom')
+  fit_pois <- fitdist(r[i,],'pois')
+  BIC_nbinom[i] <- summary(fit_nbinom)[[8]]
+  BIC_pois[i] <- summary(fit_pois)[[8]]
+}
+plot(y=BIC_pois, x=BIC_nbinom,pch=19,cex=0.5,col=rgb(0,0,0,0.5))
+abline(a=0,b=1,col=2)
